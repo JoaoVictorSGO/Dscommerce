3675ce6737faeacb13a6152a5e873334310bf3d7
Joao <jvictorsgon@gmail.com>
2025-02-01 15:34:53 -0300

    Finaliza implementação das entidades do e-commerce

--- src/main/java/com/joaovictor/commerce/entities/Order.java
+++ src/main/java/com/joaovictor/commerce/entities/Order.java
@@ -1,6 +1,9 @@
 package com.joaovictor.commerce.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -10,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -26,11 +30,14 @@ public class Order {
 	private OrderStatus status;
 	
 	@ManyToOne
-	@JoinColumn(name = "client_id")
+	@JoinColumn(name = "client_id") // Foreign key
 	private User client;
 	
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Order() {}
 
@@ -73,4 +80,22 @@ public class Order {
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	public List<Product> getProducts(){
+		return items.stream().map(x -> x.getProduct()).toList();
+	}
+	
 }
