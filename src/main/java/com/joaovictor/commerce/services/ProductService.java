51b48065288ab38da0b8b10f6e9b288f09d63e29
Joao <jvictorsgon@gmail.com>
2025-02-11 17:01:38 -0300

    Buscar os produtos paginados

--- src/main/java/com/joaovictor/commerce/services/ProductService.java
+++ src/main/java/com/joaovictor/commerce/services/ProductService.java
@@ -1,8 +1,11 @@
 package com.joaovictor.commerce.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +23,14 @@ public class ProductService {
 	@Transactional(readOnly = true) // Indicar que é um método apenas leitura.
 	public ProductDTO findById(Long id) {
 		Optional<Product> result = repository.findById(id);  // findById retorna um tipo "Optional".
-		Product product = result.orElse(new Product());// Caso retorne um 'Optional.empty()'. ele por padrão vai retornar um objeto vazio.
+		Product product = result.get();
 		ProductDTO dto = new ProductDTO(product);
 		return dto;
+	}
+	
+	@Transactional(readOnly = true) 
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> result = repository.findAll(pageable);
+		return result.map(x -> new ProductDTO(x));
 	}
 }
