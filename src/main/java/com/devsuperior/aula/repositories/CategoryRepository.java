93cd4b558a49b94e64f8836f9b214ab32c68c98d
Joao <jvictorsgon@gmail.com>
2025-07-13 10:52:12 -0300

    Remove arquivos

--- src/main/java/com/devsuperior/aula/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devsuperior.aula.repositories;
-
-import com.devsuperior.aula.entities.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
